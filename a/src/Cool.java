8bb69e0e9e0109e7b8f70502eb47d65577d6fd52
nadine <nadine@wasserstoff.creorg.local>
2013-11-25 10:23:12 +0100

    Another Commit.

--- a/src/Cool.java
+++ a/src/Cool.java
@@ -4,6 +4,7 @@ public class Cool {
 	
 	public static void main (String args[]) {
 		// That's a comment. And another change.
+		// Let's do another commit.
 		System.out.println("Git is cool");
 	}
 }
