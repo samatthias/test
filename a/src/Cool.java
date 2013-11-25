c3f01911776eabbbfb65720a71f38bc7c0dd9178
nadine <nadine@wasserstoff.creorg.local>
2013-11-25 10:24:16 +0100

    Just a local commit.

--- a/src/Cool.java
+++ a/src/Cool.java
@@ -5,6 +5,7 @@ public class Cool {
 	public static void main (String args[]) {
 		// That's a comment. And another change.
 		// Let's do another commit.
+		// another comment.
 		System.out.println("Git is cool");
 	}
 }
